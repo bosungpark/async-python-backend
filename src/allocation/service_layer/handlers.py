b6b1d3235555aef257ee9bee17a21a209d0192c3
bosung park <81157873+bosungpark@users.noreply.github.com>
2022-11-17 00:13:29 +0900

    Feat: read only view update handler

--- src/allocation/service_layer/handlers.py
+++ src/allocation/service_layer/handlers.py
@@ -70,3 +70,18 @@ def publish_allocated_event(
 ):
     with uow:
         redis_eventconsumer.publish("line_allocated", event)
+
+
+def add_allocation_to_read_model(
+        event: events.Allocated,
+        uow: unit_of_work.SqlAlchemyUnitOfWork
+):
+    with uow:
+        uow.session.execute(
+            """
+            INSERT INTO allocations_view (orderid, sku, batchref)
+            VALUES (:orderid, :sku, :batchref)
+            """,
+            dict(orderid=event.orderid, sku=event.sku, batchref=event.batchref),
+        )
+        uow.commit()
